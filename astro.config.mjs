700652e0bca7ae5c06bb65789353adc3b8c32a79
henntekosennsi1-rgb <henntekosennsi1@gmail.com>
2026-01-14 22:58:34 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://your-username.github.io',
+  site: 'https://henntekosennsi1-rgb.github.io',
   base: '/mattari-plugins',
   integrations: [mdx()],
   markdown: {
